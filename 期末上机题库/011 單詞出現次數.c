980c5ce762fc8c68b7b981754dee551ed469c0fc
墨綠BG <s5460703@gmail.com>
2023-07-17 10:28:13 +0800

    Update 011 單詞出現次數.c

--- 期末上机题库/011 單詞出現次數.c
+++ 期末上机题库/011 單詞出現次數.c
@@ -1,56 +1,78 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-int i=0;
-char *judge(char str[]);
-int main(){
-	int key=0, num[100]={0}, sum=1;
-	char strstr[1000], *substr, newstr[100][10];
-	gets(strstr);
-	substr = judge(strstr);
-	strcpy(newstr[key],substr);
-	num[key]++;
-	key++;
-	while(1){
-		substr=judge(strstr);
-		for(int j=0; j<key; j++){
-			if(strcmp(substr, newstr[j])==0){
-				num[j]++;
-				sum=0;
-				break;
-			}
-		}
-		if(sum){
-			strcpy(newstr[key], substr);
-			num[key]++;
-			key++;
-		}
-		if(strstr[i-1]=='\0') break;
-	}
-	for(i=1;i<key;i++){
-		for(int j=0;j<key-i;j++){
-			if(stricmp(newstr[j],newstr[j+1])>0){
-				char t[10];
-				strcpy(t,newstr[j]);
-				strcpy(newstr[j],newstr[j+1]);
-				strcpy(newstr[j+1],t);
-				int k;
-				k=num[j];
-				num[j]=num[j+1];
-				num[j+1]=k;
-			}
-		}
-	}
-	for(i=0;i<key;i++)
-		printf("%s   %d\n",newstr[i],num[i]);
+struct Node {
+    char word[10];
+    int count;
+    struct Node* next;
+};
+typedef struct Node Node;
+Node* create_node(char* word) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    strcpy(new_node->word, word);
+    new_node->count = 1;
+    new_node->next = NULL;
+    return new_node;
 }
-char *judge(char str[]){
-	char sub[10];
-	int j=0;
-	for(;str[i]!=' '&&str[i]!='\0';i++){
-		sub[j]=str[i];
-		j++;
-	}
-	i++;
-	sub[j]='\0';
-	return sub;
+void add_word(Node** head, char* word) {
+    if (*head == NULL) {
+        *head = create_node(word);
+        return;
+    }
+    Node* cur = *head;
+    while (cur) {
+        if (strcmp(cur->word, word) == 0) {
+            cur->count++;
+            return;
+        }
+        if (cur->next == NULL) {
+            cur->next = create_node(word);
+            return;
+        }
+        cur = cur->next;
+    }
+}
+void sort_list(Node** head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return;
+    }
+    Node* sorted = NULL;
+    Node* cur = *head;
+    while (cur) {
+        Node* prev = NULL;
+        Node* sorted_cur = sorted;
+        while (sorted_cur && strcmp(cur->word, sorted_cur->word) > 0) {
+            prev = sorted_cur;
+            sorted_cur = sorted_cur->next;
+        }
+        Node* next = cur->next;
+        if (prev == NULL) {
+            cur->next = sorted;
+            sorted = cur;
+        } else {
+            prev->next = cur;
+            cur->next = sorted_cur;
+        }
+        cur = next;
+    }
+    *head = sorted;
+}
+void print_list(Node* head){
+    while (head) {
+        printf("%s %d\n", head->word, head->count);
+        head = head->next;
+    }
+}
+int main(){
+    char str[1000];
+    fgets(str, 1000, stdin);
+    Node* head = NULL;
+    char* word = strtok(str, " \n");
+    while(word){
+        add_word(&head, word);
+        word = strtok(NULL, " \n");
+    }
+    sort_list(&head);
+    print_list(head);
+    return 0;
 }
